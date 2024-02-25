853c7e0e471ea9f2fd243c235660f6df573134a8
dijanakalaposratic <dijanakalaposratic@gmail.com>
2024-02-26 00:41:10 +0100

    created App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css';
+import Drum from './Drum';
+import { AudioClip } from './types';
 
 const audioClips: AudioClip[] = [
 	{
@@ -49,7 +51,25 @@ const audioClips: AudioClip[] = [
 ];
 
 function App() {
-	return <></>;
+	const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
+		const clip = audioClips.find((clip) => clip.keyTrigger === e.key.toUpperCase());
+		if (!clip) return;
+		(document.getElementById(clip.keyTrigger) as HTMLAudioElement).play().catch(console.error);
+
+		document.getElementById('drum-' + clip.keyTrigger)?.focus();
+		document.getElementById('display')!.innerText = clip.description;
+	};
+	return (
+		<div className='container' id='drum-machine' onKeyDown={playAudio}>
+			<h1>Drum Machine</h1>
+			<div className='whole-drum'>
+				{audioClips.map((clip) => (
+					<Drum audioClip={clip} key={clip.keyTrigger} />
+				))}
+			</div>
+			<div id='display'></div>
+		</div>
+	);
 }
 
 export default App;
